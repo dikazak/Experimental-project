f061dd6dc69762f042ffd8600f5d1a7e871b7f15
Dima Kazak <dkazak@solvd.com>
2021-02-24 15:47:53 +0300

    Update

--- src/test/java/example/SimpleTest.java
+++ src/test/java/example/SimpleTest.java
@@ -1,13 +1,20 @@
 package example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.lang.invoke.MethodHandles;
+
 public class SimpleTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Test
     public void helloWorld() {
         System.out.println("Hello, world!");
+        System.getenv().forEach((key, value) -> LOGGER.debug(key + ":" + value));
         Assert.assertTrue(true);
     }
 
