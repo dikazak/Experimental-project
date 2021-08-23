34ea320d1fa0cd5a66aae26c24dff12a966af9ad
Dima Kazak <dkazak@solvd.com>
2021-08-23 15:22:09 +0300

    Add Zephyr

--- src/test/java/example/SimpleTest.java
+++ src/test/java/example/SimpleTest.java
@@ -1,5 +1,6 @@
 package example;
 
+import com.zebrunner.agent.core.registrar.Label;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -10,32 +11,37 @@ import java.lang.invoke.MethodHandles;
 public class SimpleTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    
+
+    private final static String testCaseKey = "com.zebrunner.app/tcm.zephyr.test-case-key";
+    private final static String testCycleKey = "com.zebrunner.app/tcm.zephyr.test-cycle-key";
+
     @Test
-    public void travis() {
-        System.getenv()
-              .forEach((key, value) -> LOGGER.warn(key + ":" + value));
+    public void testCreateUser() {
+        LOGGER.info("testCreateUser logs");
+        Label.attachToTest(testCaseKey, "DEV-T1");
+        Label.attachToTestRun(testCycleKey, "DEV-R1");
+        throw new RuntimeException();
     }
 
     @Test
-    public void helloWorld() {
-        LOGGER.warn("Hello, world!");
-        LOGGER.debug(System.getenv("BUILD NUMBER"));
-        Assert.assertTrue(true);
+    public void testCreateUserMissingSomeFields() {
+        LOGGER.info("testCreateUserMissingSomeFields logs");
+        Label.attachToTest(testCaseKey, "DEV-T2");
+        Label.attachToTestRun(testCycleKey, "DEV-R1");
     }
 
     @Test
-    public void hardWorkingTest() {
-        for (int j = 0; j < 1_000_000_000; j++) {
-            Math.pow(2.0, 5);
-        }
-        Assert.assertTrue(false);
+    public void testGetUsers() {
+        LOGGER.info("testGetUsers logs");
+        Label.attachToTest(testCaseKey, "DEV-T3");
+        Label.attachToTestRun(testCycleKey, "DEV-R1");
     }
 
     @Test
-    public void failedTest() {
-        LOGGER.warn("Hello, world!");
-        Assert.assertTrue(false);
+    public void testDeleteUsers() {
+        LOGGER.info("testGetUsers logs");
+        Label.attachToTest(testCaseKey, "DEV-T4");
+        Label.attachToTestRun(testCycleKey, "DEV-R1");
     }
 
 }
