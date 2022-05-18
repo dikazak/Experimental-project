34f0adc614f73be90d162164d4c82aa451a7db8e
Dima Kazak <dkazak@solvd.com>
2022-05-18 17:13:22 +0300

    Add tests

--- src/test/java/example/TcmsSample.java
+++ src/test/java/example/TcmsSample.java
@@ -1,5 +1,6 @@
 package example;
 
+import com.zebrunner.agent.core.annotation.XrayTestKey;
 import com.zebrunner.agent.core.registrar.CurrentTestRun;
 import com.zebrunner.agent.core.registrar.Label;
 import com.zebrunner.agent.core.registrar.TestRail;
@@ -13,22 +14,25 @@ import org.testng.annotations.Test;
 
 import java.lang.invoke.MethodHandles;
 
-public class TestRailZephyrXraySample {
+public class TcmsSample {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @BeforeSuite
     public void setup() {
+        TestRail.enableRealTimeSync();
         TestRail.includeAllTestCasesInNewRun();
         TestRail.setRunId("1");
         TestRail.setAssignee("Dima");
         TestRail.setRunName("TestRun");
         TestRail.setMilestone("Milestone");
 
-        Zephyr.setTestCycleKey("DIM-1");
-        Zephyr.setJiraProjectKey("DIM");
-
-        Xray.setExecutionKey("QAP-2");
+//        Zephyr.enableRealTimeSync();
+//        Zephyr.setTestCycleKey("DIM-1");
+//        Zephyr.setJiraProjectKey("DIM");
+//
+//        Xray.enableRealTimeSync();
+//        Xray.setExecutionKey("QAP-2");
     }
 
     @Test
@@ -37,22 +41,21 @@ public class TestRailZephyrXraySample {
         //        TESTRAIL
         TestRail.setCaseId("1");
 
-        CurrentTestRun.setPlatform("Hello, World");
+//        CurrentTestRun.setPlatform("Hello, World");
 
-        Label.attachToTest("simpleLabel1", "simpleLabel1");
-        Label.attachToTest("simpleLabel2", "simpleLabel2");
+//        Label.attachToTest("simpleLabel2", "simpleLabel2");
 
         //        ZEPHYR SQUAD LABELS
-        Zephyr.setTestCaseKey("DIM-2");
+//        Zephyr.setTestCaseKey("DIM-2");
 
         //        ZEPHYR SCALE LABELS
-        Zephyr.setTestCaseKey("QAP-T1");
-        Zephyr.setTestCaseKey("QAP-T2");
-        Zephyr.setTestCaseKey("QAP-T3");
-        Zephyr.setTestCaseKey("QAP-T4");
+//        Zephyr.setTestCaseKey("QAP-T1");
+//        Zephyr.setTestCaseKey("QAP-T2");
+//        Zephyr.setTestCaseKey("QAP-T3");
+//        Zephyr.setTestCaseKey("QAP-T4");
 
         //        XRAY
-        Xray.setTestKey("QAP-3");
+//        Xray.setTestKey("QAP-3");
         throw new RuntimeException();
     }
 
